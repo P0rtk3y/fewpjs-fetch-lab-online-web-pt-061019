d08c60999ddca20fe52e76bd26350699040396da
Lana Worobec <llama.p0rtk3y@gmail.com>
2020-01-04 00:58:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-function fetchBooks('https://anapioficeandfire.com/api/books') {
-
+function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(resp => resp.json())
+    .then(json => renderBooks(json));
 }
 
 function renderBooks(json) {
